99263f9c67d6f79451af5c573c87aeab7093060c
MaxGAUBERT <120033370+MaxGAUBERT@users.noreply.github.com>
2025-11-12 17:36:58 +0100

    add filter function

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,30 @@
 import TaskList from "./Components/TaskList"
 import TaskManager from "./Components/TaskManager"
 import useTasks from "./Hooks/useTasks"
-import useModals from "./Hooks/useModals"
 import SearchBar from "./Components/SearchBar"
 import GlobalColorContextProvider from "./Contexts/GlobalColorContext"
 import EditModal from "./Components/EditModal"
-import useReminders from "./Hooks/useReminders"
-import ReminderPanel from "./Components/ReminderPanel"
 import { HiOutlineBellAlert } from "react-icons/hi2";
 
 export default function App() {
   const {
-    AddTask,
-    DeleteAllTask,
-    UpdateTask,
-    SaveTaskEdit,
-    tasks,
-    setTasks,
-    selectedTask,
-    setSelectedTask,
-    filteredTasks,
-    setFilteredTasks,
-  } = useTasks()
+  tasks,
+  filteredTasks,
+  setTasks,
+  selectedTask,
+  setSelectedTask,
+  sortedTasks,
+  showEditWindow,
+  setShowEditWindow,
+  AddTask,
+  DeleteAllTask,
+  UpdateTask,
+  SaveTaskEdit,
+  handleSearch,
+  handleSort,
+} = useTasks()
 
-  const { showEditWindow, setShowEditWindow } = useModals()
-  const hasReminder = useReminders(tasks)
 
-  const handleSearch = (query) => {
-    if (!query) {
-      setFilteredTasks(tasks)
-      return
-    }
-    const q = query.toLowerCase()
-    const results = tasks.filter(
-      (t) =>
-        t.text.toLowerCase().includes(q) ||
-        (t.describe && t.describe.toLowerCase().includes(q))
-    )
-    setFilteredTasks(results)
-  }
 
   return (
     <GlobalColorContextProvider>
@@ -52,12 +38,12 @@ export default function App() {
 
         <div className="absolute flex flex-col p-2 bg-gray-700 overflow-auto w-[1000px] h-[700px] max-h-[800px] top-0 right-0 border-4 border-gray-500 border-double rounded">
           <div className="mt-2 ml-3">
-            <SearchBar onSearch={handleSearch} tasks={tasks} />
+            <SearchBar onSearch={handleSearch} onSort={handleSort} tasks={filteredTasks} />
           </div>
           
           <div className="relative ml-5">
             <TaskList
-              tasks={filteredTasks}
+              tasks={sortedTasks}
               setTasks={setTasks}
               showEditWindow={showEditWindow}
               setShowEditWindow={setShowEditWindow}
@@ -75,8 +61,6 @@ export default function App() {
             SaveTaskEdit={SaveTaskEdit}
           />
         )}
-
-        {hasReminder && <ReminderPanel selectedTask={selectedTask}/>}
 
         <div className="fixed bottom-0">
           <TaskManager
