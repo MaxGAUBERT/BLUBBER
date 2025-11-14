ca9e75661f4399e5dfe57ba2d17a1dd03fdc503a
MaxGAUBERT <120033370+MaxGAUBERT@users.noreply.github.com>
2025-11-14 10:17:19 +0100

    save system

--- src/Components/TaskManager.jsx
+++ src/Components/TaskManager.jsx
@@ -2,14 +2,26 @@
 import { FaPlus } from 'react-icons/fa'
 import { FaMinus} from 'react-icons/fa6'
 import { FiSave } from "react-icons/fi"
+import { MdCloudUpload } from "react-icons/md";
 
-export default function TaskManager({addTask, DeleteAllTask, UpdateTask}){
+export default function TaskManager({addTask, DeleteAllTask, SaveTasks, UploadTasks}){
 
     return (
         <div>
-            <button onClick={() => addTask()}><FaPlus/></button>
-            <button onClick={() => DeleteAllTask()}><FaMinus/></button>
-            <button onClick={() => UpdateTask()}><FiSave /></button>
+            <button title='add new task' onClick={() => addTask()}><FaPlus/></button>
+            <button title="delete all tasks" onClick={() => DeleteAllTask()}><FaMinus/></button>
+            <button title="save tasks" onClick={() => SaveTasks()}><FiSave /></button>
+            <span></span>
+            <button className="ml-5" title="upload tasks" onClick={() => {document.getElementById("uploadTasksInput").click()}}><MdCloudUpload /></button>
+
+            <input
+                type="file"
+                accept="application/json"
+                id="uploadTasksInput"
+                className="hidden"
+                onChange={UploadTasks}
+            />
+
         </div>
     )
 }
